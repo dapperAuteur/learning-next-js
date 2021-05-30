1df72a94ebd4ee16a626c8e31485bac29e8ddea1
awe <a@awews.com>
2021-05-30 16:29:04 -0700

    connects client http request to api function to create new meetup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb'
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
         const {title, image, address, description} = data;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,17 @@
 import NewMeetupForm from './../../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(`enteredMeetupData`, enteredMeetupData)
+    async function addMeetupHandler(enteredMeetupData) {
+        // console.log(`enteredMeetupData`, enteredMeetupData)
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(`data`, data)
     }
     return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
 }
