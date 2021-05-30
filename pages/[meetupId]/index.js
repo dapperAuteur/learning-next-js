1a6dbf851843e8662c40e677c97117c5c5812980
awe <a@awews.com>
2021-05-30 12:38:14 -0700

    input skeleton for meetup details component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function MeetupDetils() {
+    return <Fragment>
+        <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*' alt='moscow-russia-st-basil-cathedral'/>
+        <h1>First Meetup</h1>
+        <address>Address</address>
+        <p>Description</p>
+    </Fragment>;
+}
+
+export default MeetupDetils;
