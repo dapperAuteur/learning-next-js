3d23c1c2805bff3f6b0166cc79af1b72e8aa3f8c
awe <a@awews.com>
2021-05-30 15:15:02 -0700

    refactor to use getStaticProps to render data dynamically
    creates bug that requires getStaticPaths to resolve

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,25 @@
 import MeetupDetail from './../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
-    return <MeetupDetail image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*" title="First Meetup" address="address" description="description" />
+function MeetupDetails(props) {
+    return <MeetupDetail image={props.image} title={props.title} address={props.address} description={props.description} />
+}
+
+
+export async function getStaticProps(context) {
+    // fetch data for single meetup
+    const meetupId = context.params.meetupId;
+    console.log(`meetupId`, meetupId)
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*',
+                title: 'First Meetup',
+                address: 'address',
+                description: 'description'
+            }
+        }
+    }
 }
 
 export default MeetupDetails;
