e2cb2ff6f05cbea043c9347737eb72161bfe735d
awe <a@awews.com>
2021-05-30 15:35:04 -0700

    change code to match video

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,8 @@
 import MeetupDetail from './../../components/meetups/MeetupDetail';
 
 function MeetupDetails(props) {
-    return <MeetupDetail image={props.image} title={props.title} address={props.address} description={props.description} />
+    // return <MeetupDetail image={props.image} title={props.title} address={props.address} description={props.description} />
+    return <MeetupDetail image='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*' title='First Meetup' address='address' description='description' />
 }
 
 export async function getStaticPaths(){
