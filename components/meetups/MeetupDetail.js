dbe8b01efad51b2e452b73e1620315cf7dbcfb57
awe <a@awews.com>
2021-05-30 12:51:34 -0700

    refactor to style using css modules and remove fragment element

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,13 +1,13 @@
-import { Fragment } from 'react'
+import classes from './MeetupDetail.module.css';
 
 function MeetupDetail(props) {
     return (
-        <Fragment>
-        <img src={props.image} alt={props.title}/>
-        <h1>{props.title}</h1>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-    </Fragment>
+        <section className={classes.detail}>
+            <img src={props.image} alt={props.title}/>
+            <h1>{props.title}</h1>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </section>
     )
 }
 
