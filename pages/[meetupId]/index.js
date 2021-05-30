4b67a20e729ec99bae9b8659819b1c6b417a38cd
awe <a@awews.com>
2021-05-30 12:48:48 -0700

    refactor to move fragment to MeetupDetail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,7 @@
-import { Fragment } from "react";
+import MeetupDetail from './../../components/meetups/MeetupDetail';
 
-function MeetupDetils() {
-    return <Fragment>
-        <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*' alt='moscow-russia-st-basil-cathedral'/>
-        <h1>First Meetup</h1>
-        <address>Address</address>
-        <p>Description</p>
-    </Fragment>;
+function MeetupDetails() {
+    return <MeetupDetail image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/moscow-russia-st-basil-cathedral-royalty-free-image-1594133515.jpg?crop=1xw:1xh;center,top&resize=768:*" title="First Meetup" address="address" description="description" />
 }
 
-export default MeetupDetils;
+export default MeetupDetails;
