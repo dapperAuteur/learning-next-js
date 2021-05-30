5281c567ba92f0f100b26300049d81753a37b210
awe <a@awews.com>
2021-05-30 16:08:08 -0700

    add mongodb connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,23 @@
+import {MongoClient} from 'mongodb'
+
 function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
         const {title, image, address, description} = data;
         console.log(`data`, data)
+
+        console.log(`process.env.ATLAS_URI`, process.env.ATLAS_URI)
+        const client = await MongoClient.connect(process.env.ATLAS_URI)
+        const db = client.db()
+
+        const meetupsCollections = db.collection('meetups');
+
+       const result = await meetupsCollections.insertOne(data)
+
+       console.log(`result`, result)
+       client.close()
+
+       res.status(201).json({message: 'Meetup inserted'})
     }
 }
 
